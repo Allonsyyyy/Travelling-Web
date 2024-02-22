6fe8823846d49e0ba3bd37369f4e456fa94cf8fe
Hercules3006 <136312056+Hercules3006@users.noreply.github.com>
2024-02-22 20:52:46 +0700

    Update signup.js

--- Travelling-Web-frontend/Travelling-Web-frontend/js/signup.js
+++ Travelling-Web-frontend/Travelling-Web-frontend/js/signup.js
@@ -1,13 +1,14 @@
-// validation form register and register user local storage
+// xác thực đăng ký và đăng ký người dùng lưu trữ cục bộ
 const inputUsernameRegister = document.querySelector(".input-signup-username");
 const inputPasswordRegister = document.querySelector(".input-signup-password");
 const inputPassword2Register = document.querySelector(".input-signup-password2");
 const btnRegister = document.querySelector(".signup__signInButton");
 
-// validation form register and register user local storage
+//xác thực đăng ký và đăng ký người dùng lưu trữ cục bộ
 
 btnRegister.addEventListener("click", (e) => {
     e.preventDefault();
+    // tránh để trống chỗ nhập
     if (
         inputUsernameRegister.value === "" ||
         inputPasswordRegister.value === "" ||
@@ -16,14 +17,15 @@ btnRegister.addEventListener("click", (e) => {
     ) {
         alert("Vui lòng không để trống");
     }
+    // xác nhận mật khẩu
     if (inputPassword2Register.value !== inputPasswordRegister.value) {
         alert("Mật khẩu không trùng khớp");
     } else {
-        // array user
         const user = {
             username: inputUsernameRegister.value,
             password: inputPasswordRegister.value,
         };
+        // chuyển thành  chuỗi json
         let json = JSON.stringify(user);
         localStorage.setItem(inputUsernameRegister.value, json);
         alert("Đăng Ký Thành Công");
