c510f2c255d407c0626dc68227c05498928a1fd9
Hercules3006 <136312056+Hercules3006@users.noreply.github.com>
2024-02-22 20:48:27 +0700

    Update destination.js

--- Travelling-Web-frontend/Travelling-Web-frontend/js/destination.js
+++ Travelling-Web-frontend/Travelling-Web-frontend/js/destination.js
@@ -1,30 +1,32 @@
 let data = null;
 
 const fetchData = (jsonData) => {
-    data = jsonData;
-    addElement(data.slice(0, currentLoad));
-}
-
+        data = jsonData;
+        addElement(data.slice(0, currentLoad));
+    }
+    // lấy dữ liệu từ file json truyền vào data để sử dụng
 fetch('../data/des.json')
     .then((response) => response.json())
     .then((json) => { fetchData(json); });
 
 
 const removeExistAnimation = () => {
-    CardAnimation = document.getElementsByClassName('card-animation');
-    for (let i = 0; i < CardAnimation.length; i++) {
-        CardAnimation[i].style.animation = 'none';
+        CardAnimation = document.getElementsByClassName('card-animation');
+        for (let i = 0; i < CardAnimation.length; i++) {
+            CardAnimation[i].style.animation = 'none';
+        }
     }
-}
-
+    // đọc dữ liệu trong data
 const addElement = (data) => {
 
-    removeExistAnimation();
+        removeExistAnimation();
 
-    let html = document.getElementsByClassName('des-list')[0].innerHTML;
+        // dùng render để tạo dữ liệu lên màn hình 
+        let html = document.getElementsByClassName('des-list')[0].innerHTML;
 
-    data.forEach(element => {
-        html += `
+        data.forEach(element => {
+                        // đọc cái thuộc tính trong data thông qua cú pháp ${element.}
+            html += `
                 <li class="card-animation" >
                     <a href="${"detail-destination.html?id=" + element.id}" class="destination-card" >
                         <div class="card-top">
@@ -42,9 +44,11 @@ const addElement = (data) => {
                     </a>
                 </li>
             `
-    });
-    document.getElementsByClassName('des-list')[0].innerHTML = html;
-}
+        });
+        document.getElementsByClassName('des-list')[0].innerHTML = html;
+                //đổ thông tin vào thẻ des-list
+    }
+    // xử lí nút loadmore
 
 let currentLoad = 3;
 
@@ -52,6 +56,7 @@ const LoadMoreButton = document.getElementById('loadMore');
 
 LoadMoreButton.addEventListener('click', () => {
     currentLoad += 3;
+        //mỗi lần click sẽ sinh ra thêm 3 thẻ
     addElement(data.slice(currentLoad - 3, currentLoad));
     if (currentLoad >= data.length) {
         LoadMoreButton.style.display = 'none';
